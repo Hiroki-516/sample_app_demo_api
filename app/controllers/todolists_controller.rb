ed374a793ffdbbbca55d4449c525dbd87e8d8091
Hiroki-516 <141198036+Hiroki-516@users.noreply.github.com>
2023-11-01 12:13:17 +0000

    pyson-language-API

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,52 @@
+class TodolistsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    # 投稿した本文を Language.get_data(list_params[:body]) でAPI側に渡しています。その後、API側から返ってきた値をもとに、スコアを作成。
+    @list.score = Language.get_data(list_params[:body])
+    # 投稿した画像を Vision.get_image_data(list_params[:image]) でAPI側に渡しています。
+    tags = Vision.get_image_data(list_params[:image])
+    if @list.save
+      tags.each do |tag|
+        @list.tags.create(name: tag)
+      end
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+
+end
